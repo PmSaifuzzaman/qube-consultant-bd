56ce733e707c24ebd33ef2b5ddd6022293038cf2
Saifuzzaman <pmsaifuzzaman@gmail.com>
2024-02-21 23:53:05 +0600

    Building section added

--- src/pages/Building/Building.jsx
+++ src/pages/Building/Building.jsx
@@ -4,15 +4,20 @@ import img3 from "../../assets/Building/3.jpg"
 import img4 from "../../assets/Building/4.jpg"
 import img5 from "../../assets/Building/kiam 4.jpg"
 import img6 from "../../assets/Building/kiam ren 7.jpg"
+import img7 from "../../assets/Building/7.png"
+import img8 from "../../assets/Building/8.png"
+import img9 from "../../assets/Building/9.png"
+import img10 from "../../assets/Building/10.jpg"
 import Footer from "../../components/Footer/Footer"
+import Navbar from "../../components/Navbar/Navbar"
 
 
 
 const Building = () => {
     return (
         <div>
-            
-            <div className=" building-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+            <Navbar></Navbar>
+            <div className="pt-32 p-5 building-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
                 {/* Card 1 */}
                 <div className="card bg-base-100 shadow-xl">
                     <figure className="px-5 pt-5">
@@ -26,7 +31,7 @@ const Building = () => {
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
@@ -43,7 +48,7 @@ const Building = () => {
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
@@ -60,7 +65,7 @@ const Building = () => {
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
@@ -77,7 +82,7 @@ const Building = () => {
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
@@ -94,7 +99,7 @@ const Building = () => {
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
@@ -111,58 +116,75 @@ const Building = () => {
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
-                {/* Card 1 */}
+                {/* Card 7 */}
                 <div className="card bg-base-100 shadow-xl">
                     <figure className="px-5 pt-5">
-                        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                        <img src={img7} alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
+                        <h2 className="card-title">Office Building Design & Development</h2>
                         <div className="rating rating-md">
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
-                {/* Card 1 */}
+                {/* Card 8 */}
                 <div className="card bg-base-100 shadow-xl">
                     <figure className="px-5 pt-5">
-                        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                        <img src={img8} alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
+                        <h2 className="card-title">Office Building Design & Development</h2>
                         <div className="rating rating-md">
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
-                {/* Card 1 */}
+                {/* Card 9 */}
                 <div className="card bg-base-100 shadow-xl">
                     <figure className="px-5 pt-5">
-                        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                        <img src={img9} alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
+                        <h2 className="card-title">Office Building Design & Development</h2>
                         <div className="rating rating-md">
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-                            <p className="pl-2">5.0 ( 3 Reviews )</p>
+                            <p className="pl-1">5.0 (3 Reviews)</p>
+                        </div>
+                    </div>
+                </div>
+                {/* Card 10 */}
+                <div className="card bg-base-100 shadow-xl">
+                    <figure className="px-5 pt-5">
+                        <img src={img10} alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Office Building Design & Development</h2>
+                        <div className="rating rating-md">
+                            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
+                            <p className="pl-1">5.0 (3 Reviews)</p>
                         </div>
                     </div>
                 </div>
